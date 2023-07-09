b4d2873e87f60d499e71b4f56cb12c99f13f8af5
irysik123 <vysotskaya.iryna@gmail.com>
2023-07-09 13:29:47 -0400

    task 2 + styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,52 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import contacts from './data/contacts.json'
+import contacts from './data/contacts.json';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { Title } from './App.styled'
+
+
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: [],
+    contacts: contacts,
+    filter: '',
   };
 
-  addContact = text => {
-    const newContact = {
-      id: nanoid(),
-      name: text,
+  handleFilterChange = e => {
+    this.setState({ filter: e.target.value });
+  };
 
+  handleAdd = newContact => {
+    if(this.state.contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
+      alert(`${newContact.name} is already in contacts`)
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [newContact, ...contacts],
+      }));
     }
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts]
-    }))
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-
-    this.props.onSubmit(this.state);
-
-    this.reset();
-  };
-
-  handleChange = e => {
-    this.setState({ name: e.currentTarget.value });
-  };
-
-  reset = () => {
-    this.setState({ name: [] });
-  };
-
+  onDeleteContact = id => {
+    let indexToDelete = this.state.contacts.findIndex(contact => contact.id === id)
+    let newContacts = this.state.contacts
+    newContacts.splice((indexToDelete), 1)
+    this.setState({contacts: [...newContacts]})
+  }
+ 
   render() {
     return (
       <div>
-        <h1>Phonebook</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-            id={nanoid()}
-          />
-          <button type="submit" onSubmit={this.addContact}>Add contact</button>
-        </form>
-        <ul>
-          {contacts.map(({ id, name, number }) => (
-            <li key={id}>
-              <p>{name}</p>
-              <p>{number}</p>
-              <button type="button" /* onClick={onDeleteContact} */>
-                Delete contact
-              </button>
-            </li>
-          ))}
-        </ul>
+        <Title>Phonebook</Title>
+         <ContactForm handleAdd={this.handleAdd}/>
+
+        <Title>Contacts</Title>
+        <Filter handleFilterChange={this.handleFilterChange} value={this.state.filter}/>
+        <ContactList list={this.state.contacts
+            .filter(user =>
+              user.name.toLowerCase().includes(this.state.filter.toLowerCase())
+            )}
+            onDeleteContact={this.onDeleteContact}/>
       </div>
     );
   }
